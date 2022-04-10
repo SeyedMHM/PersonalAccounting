fcdbe5a9c9a2833fd26dab3fd85dafb7cebe92ed
MohammadHossein <pbita.atefi7@gmail.com>
2022-04-10 18:29:18 +0430

    add shared common and api project

---
+++ Shared/PersonalAccounting.Shared.Common/Utilities/ApiStatusCode.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalAccounting.Shared.Common.Utilities
+{
+    public enum ApiResultStatusCode
+    {
+        [Display(Name = "عملیات با موفقیت انجام شد")]
+        OK = 200,
+
+        [Display(Name = "پارامتر های ارسالی معتبر نیستند")]
+        BadRequest = 400,
+
+        [Display(Name = "خطای احراز هویت")]
+        Unauthorized = 401,
+
+        [Display(Name = "اطلاعاتی با آدرس ارسالی یافت نشد")]
+        NotFound = 404,
+
+        [Display(Name = "خطایی در سرور رخ داده است")]
+        InternalServerError = 500,
+    }
+}
