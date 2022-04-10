8c3af45efea7e78c2d548be39345008358e4fb97
MohammadHossein <pbita.atefi7@gmail.com>
2022-04-10 18:29:18 +0430

    add shared common and api project

---
+++ Shared/PersonalAccounting.Shared.Common/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonalAccounting.Shared.Common.Entites
+{
+    public interface IEntity
+    {
+    }
+    
+    public abstract class BaseEntity<KeyType> : IEntity
+    {
+        public KeyType Id { get; set; }
+    }
+
+    public abstract class BaseEntity : BaseEntity<int>
+    {
+    }
+}
